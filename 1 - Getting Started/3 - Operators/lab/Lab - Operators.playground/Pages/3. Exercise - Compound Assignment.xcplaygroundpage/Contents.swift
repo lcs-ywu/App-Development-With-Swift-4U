a5e4d1bb52cd266a0a5b19bbaa69131671a29849
James Wu <ywu@lcs.on.ca>
2019-10-19 23:55:22 -0400

    finish P3

--- 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
@@ -3,7 +3,9 @@
  
  Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
  */
-
+var number = 10
+number += 5
+number *= 2
 
 /*:
  Create a variable called `piggyBank` that begins at 0. You will use this to keep track of money you earn and spend. For each point below, use the right compound assignment operator to update the balance in your piggy bank.
@@ -16,7 +18,12 @@
  
  Print the balance of your piggy bank after each step.
  */
-
+var piggyBank = 0
+piggyBank += 10
+piggyBank += 20
+piggyBank /= 2
+piggyBank *= 3
+piggyBank -= 3
 
 
 
