eaa4d4483418bfcce3185ab1a60faf382908dd66
James Wu <ywu@lcs.on.ca>
2019-10-20 00:26:49 -0400

    finish P7

--- 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
@@ -1,18 +1,21 @@
-/*:
- ## Exercise - Numeric Type Conversion
+//First Command Lost
+//let x = 10
+//let y = 3.2
+//let multipliedAsIntegers = x*y
+//print (multipliedAsIntegers)
 
- Create an integer constant `x` with a value of 10, and a double constant `y` with a value of 3.2. Create a constant `multipliedAsIntegers` equal to `x` times `y`. Does this compile? If not, fix it by converting your `Double` to an `Int` in the mathematical expression. Print the result.
- */
+let x:Double = 10
+let y = 3.2
+let multipliedAsDouble = x*y
+print (multipliedAsDouble)
 
 
-/*:
- Create a constant `multipliedAsDoubles` equal to `x` times `y`, but this time convert the `Int` to a `Double` in the expression. Print the result.
- */
+//Create a constant `multipliedAsDoubles` equal to `x` times `y`, but this time convert the `Int` to a `Double` in the expression. Print the result. = <#value#>
 
 
 /*:
  Are the values of `multipliedAsIntegers` and `multipliedAsDoubles` different? Print a statement to the console explaining why.
  */
-
+//No,because 'Int' is changed into 'Double'
 
 //: [Previous](@previous)  |  page 7 of 8  |  [Next: App Exercise - Converting Types](@next)
