bb16e2db8b6d296d52d1793915c29792b59ff168
James Wu <ywu@lcs.on.ca>
2019-11-13 11:12:20 -0500

    Some small changes

--- lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@ let name = "James"
  - callout(Example): If your favorite quote is "The grass is always greener on the other side" the value of `favoriteQuote` should be such that printing `favoriteQuote` results in the following:
     * `My favorite quote is "The grass is always greener on the other side."`
  */
-let favoriteQuote = "My favorite quote is \"I'm the best\"."
+let favoriteQuote = "My favorite quote is \"I'm the best.\"."
 print(favoriteQuote)
 /*:
  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
